a1c3e60af83ebd28e7e9baca083bc7eedf74642b
Luis C. Gutierrez <luis.c.gutierrez.h@gmail.com>
2015-03-30 21:39:51 -0600

    Added Ackley function to test

--- test_AD.m
+++ test_AD.m
@@ -1,4 +1,17 @@
+%
+% Test Rosenbrock function
+%
+
 f = @(x) (1-x(1))^2+100*(x(2)-x(1)^2)^2;
 x=AD([1,2]);
+y=f(x);
+disp(y);
+
+%
+% Test Ackley function
+%
+f = @(x) -20*exp(-0.2*sqrt(0.5*(x(1)^2+x(2)^2))) ...
+            -exp(0.5*(cos(2*pi*x(1))+cos(2*pi*x(2))))+20+exp(1);
+x = AD([1,2]);
 y=f(x);
 disp(y);
